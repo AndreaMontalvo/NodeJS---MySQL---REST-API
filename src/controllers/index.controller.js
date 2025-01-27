7de79aa894169c64e15ca08edfd276f6c92c9b5a
Andrea Montalvo <lc.cm.montalvo@gmail.com>
2025-01-27 19:49:18 -0400

    Index HTLM added

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,6 +1,20 @@
 import { pool } from "../db.js";
 
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
+
 export const ping = async (req, res) => {
     const [result] = await pool.query("SELECT 1 + 1 as result");
     res.json(result)
+}
+
+export const index = (req, res) => {
+    const camino = path.join(__dirname, '..', "index.html")
+    res.sendFile(`${camino}`)
+
+    
 }
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { ping } from "../controllers/index.controller.js";
+import { ping, index } from "../controllers/index.controller.js";
 
 const router = Router()
 
@@ -8,9 +8,8 @@ const router = Router()
 /*CHEQUEO DE CONEXION */
 router.get("/ping", ping)
 
-router.get("/pepe",  (req, res) => {
-    res.redirect("https://roadmap.sh/frontend")
-})
+/*PAGINA PRINCIPAL (INDEX) */
+router.get("/",  index)
 
 
 
